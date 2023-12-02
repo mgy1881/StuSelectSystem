a85334ca9eca6771652a5408e9a67422c97fef8c
ygm <2302253692@qq.com>
2023-12-03 00:15:53 +0800

    添加了实现jwt校验的一些必要依赖与文件，修改了数据表结构，添加了密码字段，暂未实现功能

--- StuSelectSystem/src/test/java/com/web/StuSelectSystemApplicationTests.java
+++ StuSelectSystem/src/test/java/com/web/StuSelectSystemApplicationTests.java
@@ -1,13 +1,31 @@
 package com.web;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+//@SpringBootTest
 class StuSelectSystemApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void JwtTest(){
+        Map<String,Object> claims = new HashMap<>();
+        claims.put("id",1);
+        String jwt = Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, "123456")
+                .addClaims(claims)
+                .setExpiration(new Date(System.currentTimeMillis() + 3600 * 1000))
+                .compact();
+        System.out.println(jwt);
     }
 
 }
